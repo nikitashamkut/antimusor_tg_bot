97255d897ebb20d5b6e12d05dda47c84da888146
Nikita Shamkut <nikitashamkut@gmail.com>
2021-01-08 17:17:02 +0300

    feat: production mode

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ import WizardScene from "telegraf/scenes/wizard/index.js";
 import { botMenu } from "./botFunctions/botMenu.js";
 import { reportScene } from "./botFunctions/botReport.js";
 import { reportData, menuData, helpData } from "./botFunctions/botData.js";
+const { URL } = process.env;
+const { PORT } = process.env.PORT || 5000;
 
 export const telegramBot = async () => {
   const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
@@ -40,5 +42,10 @@ export const telegramBot = async () => {
     ctx.reply(helpData.helpText).then(() => botMenu(ctx));
   });
 
-  bot.launch();
+  if (process.env.NODE_ENV === "production") {
+    bot.telegram.setWebhook(`${URL}/antimusor_bot`);
+    bot.startWebhook("/secret-path", null, PORT);
+  } else {
+    bot.launch();
+  }
 };
