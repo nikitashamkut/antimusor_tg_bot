3838356d8f447945f8958f4a66ce221218f773cc
Nikita Shamkut <nikitashamkut@gmail.com>
2021-01-08 18:00:09 +0300

    fix: deployment mode

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ import { botMenu } from "./botFunctions/botMenu.js";
 import { reportScene } from "./botFunctions/botReport.js";
 import { reportData, menuData, helpData } from "./botFunctions/botData.js";
 const { URL } = process.env;
-const { PORT } = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 export const telegramBot = async () => {
   const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
@@ -44,7 +44,7 @@ export const telegramBot = async () => {
 
   if (process.env.NODE_ENV === "production") {
     bot.telegram.setWebhook(`${URL}/antimusor_bot`);
-    bot.startWebhook("/secret-path", null, PORT);
+    bot.startWebhook(`${URL}/antimusor_bot`, null, PORT);
   } else {
     bot.launch();
   }
